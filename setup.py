c28980e34d011b06b5c0c24e2c38a94ab6cd055b
Pavlo Pukach <pavlopukach@gmail.com>
2016-12-12 00:24:51 +0200

    Published to PyPi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='runge_kutt',
+      version='0.1',
+      description='A differential equation solving algorithm implementation',
+      url='http://github.com/Icebreaker454/runge_kutt',
+      author='Paul Pukach a.k.a. Icebreaker',
+      author_email='pavlopukach@gmail.com',
+      license='MIT',
+      packages=['runge_kutt'],
+      zip_safe=False)
